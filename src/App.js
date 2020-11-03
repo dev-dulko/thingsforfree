b19cd341653c85655313894624a088b19e2d1f81
Kamil Duliniec <duliniec.kamil@gmail.com>
2020-11-03 15:12:04 +0100

    create repo, config environment and structure

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Home from "./components/Home/Home";
+import Form from "./components/Form";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Logout from "./components/Logout";
+import NotFound from "./components/NotFound";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/oddaj-rzeczy" component={Form} />
+        <Route path="/logowanie" component={Login} />
+        <Route path="/rejestracja" component={Register} />
+        <Route path="/wylogowano" component={Logout} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+}
